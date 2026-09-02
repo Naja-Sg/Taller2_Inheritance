bd882e7307fcf4dd8d1880804f607e36b9697a3c
Najabi Salgado <najabisalgadogiraldo@gmail.com>
2026-09-02 16:22:12 -0500

    renamed wrong disk folders after previous migration when changing the repo

---
+++ Taller3_Inheritance/Backend/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend;
+
+public class Rectangle : Square
+{
+
+    //Fields
+
+    // Constructors
+    public Rectangle(string name, decimal a) : base(name, a)
+    {
+    }
+
+    //Properties
+
+    //Public Methods
+
+    //Private methods
+}
