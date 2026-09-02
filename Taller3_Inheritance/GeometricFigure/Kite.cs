9cc66a2a0fe1d4e67d4ece240e8f67b8b4adaa23
Najabi Salgado <najabisalgadogiraldo@gmail.com>
2026-09-02 16:03:16 -0500

    Kite defining

--- Taller3_Inheritance/GeometricFigure/Kite.cs
+++ Taller3_Inheritance/GeometricFigure/Kite.cs
@@ -9,14 +9,50 @@ public class Kite : Rhombus
 
     //Fields
 
+    private decimal _b;
+
     //Constructors
-    public Kite(string name, decimal a, decimal d1, decimal d2) : base(name, a, d1, d2)
+    public Kite(string name, decimal a, decimal d1, decimal d2, decimal b) : base(name, a, d1, d2)
     {
+        B = b;
     }
 
-    //PRoperties
+
+    //Properties
+
+    public decimal B 
+    {
+        get => _b; 
+        set => _b = ValidateB(value); 
+    }
 
     //Public Methods
 
+        public override decimal GetArea()
+    {
+        decimal area = (D1 *  D2)/2;
+        return area;
+    }
+
+    public override decimal GetPerimeter()
+    {
+        decimal perimeter = 2 * (A+B);
+        return perimeter;
+    }
+
     //Private Methods
+
+    private decimal ValidateB(decimal b)
+    {
+        if (b < 0)
+        {
+            throw new Exception($"The side length: {b}, is not valid."); ;
+        }
+
+        else
+        {
+            return b;
+        }
+
+    }
 }
